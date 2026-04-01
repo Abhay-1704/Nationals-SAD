baa5ada7e8d8d331f5c82e204140832e06b9a5c2
Abhay-1704 <abhaysps2004@gmail.com>
2026-04-01 07:30:53 +0530

    Testing

---
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using MyApp;
+
+namespace MyApp.Tests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUser_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            var result = reservation.CanBeCancelledBy(user);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUser_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ MyApp/MyApp/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            return user.IsAdmin || user == MadeBy;
+        }
+    }
+}
